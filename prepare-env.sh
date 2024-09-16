5b53158d5481eef06d8bf2ec1b88fe34f3964df1
Pengyu Nie <prodigy.sov@gmail.com>
2024-09-16 01:08:45 -0400

    including the first demo

---
+++ prepare-env.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+
+function prepare_conda_env() {
+  # the python version to use
+  local python_version=${1:-3.10}
+  shift
+  # the conda env name
+  local env_name=${1:-cs846mlse}
+  shift
+
+  echo ">>> Preparing conda environment \"${env_name}\", python_version=${python_version}"
+
+  # Preparation
+  set -e
+  eval "$(conda shell.bash hook)"
+  conda env remove --name $env_name
+  conda create --name $env_name python=$python_version pip -y
+  conda activate $env_name
+  pip install --upgrade pip
+
+  # Install libraries
+  # TODO: (optional) install PyTorch if you use it, preferably using conda; check https://pytorch.org/get-started/locally/ for the latest command
+  pip install -r requirements.txt
+  # pip install -e .[dev]
+}
+
+prepare_conda_env $@
